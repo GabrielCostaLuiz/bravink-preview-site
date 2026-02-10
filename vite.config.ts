2ea270bbaddc53ce6a194d329e6c9b10247884a6
Gabriel Costa Luiz <gabrielbragacostaluiz@gmail.com>
2026-02-10 14:48:56 -0300

    Configurar deploy para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are linked correctly on GitHub Pages
-  build: {
-    outDir: 'dist',
-  },
+  base: "/bravink-preview-site/", // Ensures assets are linked correctly on GitHub Pages
 });
